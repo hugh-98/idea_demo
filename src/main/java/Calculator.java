9bb12afc7402c2f9d06e45ccb2806aab19633d4c
hugh98 <822051253@qq.com>
2023-03-23 09:28:46 +0800

    add caclulator test

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -5,6 +5,13 @@
 public class Calculator {
 
     public int compute(int num1, int num2, String symbol) {
-        return 0;
+        switch (symbol) {
+            case "+":
+                return num1 + num2;
+            case "-":
+                return num1 - num2;
+            default:
+                throw new IllegalArgumentException();
+        }
     }
 }
